7a97bc5d9ec8f314d7a98ea07c4cab9b51c21c1d
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-03-24 22:33:17 +0430

    add projects page

---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+
+export const Projects = () => {
+    return (
+        <div className="container">
+            <div>
+                This is Projects page.
+            </div>
+        </div>
+    )
+}
+export default Projects
